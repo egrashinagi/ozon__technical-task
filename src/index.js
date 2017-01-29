1a1b9a17693d3fc3494e9439014569877f469a71
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2017-01-29 23:58:04 +0300

    media-list: added reques data for component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 'use strict';
 
-import './index.scss';
+import './index.scss'; // main styles
+
+// components
+import MediaList from './components/media-list'
+
+
+let req = {
+  url: 'https://api.instagram.com/v1/users/691623/media/recent',
+  params: {
+    access_token: '691623.1419b97.479e4603aff24de596b1bf18891729f3',
+    count: 20,
+  },
+};
+
+let requestParams = '?';
+for (let param in req.params) {
+  requestParams += `${param}=${req.params[param]}&`;
+}
+
+
+fetch(req.url + requestParams)
+  .then(result => {
+    return result.json();
+  })
+  .then(response => {
+    new MediaList({
+      element: document.querySelector('media-list[data-name="main"]'),
+      data: response.data
+    });
+  })
+;
+
 
 console.log('index.js - load');
